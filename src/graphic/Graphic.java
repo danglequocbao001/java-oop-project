b2dc97e90fdbeb1125ddc51a1f31c228703c9692
Bao Dang <danglequocbao001@gmail.com>
2021-11-28 01:53:42 +0700

    feat: docgia

--- src/graphic/Graphic.java
+++ src/graphic/Graphic.java
@@ -1,7 +1,6 @@
 package graphic;
 
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
@@ -40,15 +39,14 @@ public class Graphic {
     public void mainMenu() {
         mainFrame.setVisible(false);
         prepareGUI();
-        JLabel hoTen = new JLabel("", JLabel.CENTER);
-        JLabel MSSV = new JLabel("", JLabel.CENTER);
-        JLabel lop = new JLabel("", JLabel.CENTER);
-        GraphicFunctions.setLabel(hoTen, contentPane, "Đặng Lê Quốc Bảo - Nguyễn Hữu Phước", 70, 10, 300, 50);
+        JLabel hoTen = new JLabel();
+        JLabel MSSV = new JLabel();
+        JLabel lop = new JLabel();
+        GraphicFunctions.setLabel(hoTen, contentPane, "Họ tên: Đặng Lê Quốc Bảo - Nguyễn Hữu Phước", 70, 10, 300, 50);
         GraphicFunctions.setLabel(MSSV, contentPane, "MSSV: N19DCCN014 - N19DCCN145", 70, 40, 300, 50);
         GraphicFunctions.setLabel(lop, contentPane, "Lớp: D19CQCN02-N - D19CQCN03-N", 70, 70, 300, 50);
 
         JButton DocGia = new JButton();
-        JButton DauSach = new JButton();
         JButton DanhMucSach = new JButton();
         JButton MuonTraSach = new JButton();
         JButton Thoat = new JButton();
@@ -57,13 +55,6 @@ public class Graphic {
         DocGia.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 docGia();
-            }
-        });
-
-        GraphicFunctions.setButton(DauSach, contentPane, "ĐẦU SÁCH", 550, 120, 200, 50);
-        DauSach.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("clicked");
             }
         });
 
@@ -81,7 +72,7 @@ public class Graphic {
             }
         });
 
-        GraphicFunctions.setButton(Thoat, contentPane, "THOÁT", 120, 250, 200, 50);
+        GraphicFunctions.setButton(Thoat, contentPane, "THOÁT", 350, 400, 200, 50);
         Thoat.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 mainFrame.setVisible(false);
@@ -91,44 +82,85 @@ public class Graphic {
         mainFrame.setVisible(true);
     }
 
+    // public void docGia() {
+    // mainFrame.setVisible(false);
+    // prepareGUI();
+    // JButton DanhSachDocGia = new JButton();
+    // JButton ChinhSuaDocGia = new JButton();
+    // JButton QuayLai = new JButton();
+
+    // GraphicFunctions.setButton(DanhSachDocGia, contentPane, "DANH SÁCH ĐỘC GIẢ",
+    // 350, 120, 200, 50);
+    // DanhSachDocGia.addActionListener(new ActionListener() {
+    // public void actionPerformed(ActionEvent e) {
+    // danhSachDocGia();
+    // }
+    // });
+
+    // GraphicFunctions.setButton(ChinhSuaDocGia, contentPane, "CHỈNH SỬA ĐỘC GIẢ ",
+    // 350, 225, 200, 50);
+    // ChinhSuaDocGia.addActionListener(new ActionListener() {
+    // public void actionPerformed(ActionEvent e) {
+    // System.out.println("ChinhSuaDocGia");
+    // }
+    // });
+
+    // GraphicFunctions.setButton(QuayLai, contentPane, "QUAY LẠI", 350, 330, 200,
+    // 50);
+    // QuayLai.addActionListener(new ActionListener() {
+    // public void actionPerformed(ActionEvent e) {
+    // mainMenu();
+    // }
+    // });
+    // }
+
     public void docGia() {
         mainFrame.setVisible(false);
         prepareGUI();
-        JButton ThemDocGia = new JButton();
-        JButton DanhSachDocGia = new JButton();
-        JButton XoaDocGia = new JButton();
-        JButton ChinhSuaDocGia = new JButton();
+
+        JLabel themDocGia = new JLabel();
+        JLabel maThe = new JLabel();
+        JLabel ho = new JLabel();
+        JLabel ten = new JLabel();
+        JLabel gioiTinh = new JLabel();
+        JTextField nhapMaThe = new JTextField();
+        JTextField nhapHo = new JTextField();
+        JTextField nhapTen = new JTextField();
+        JTextField nhapGioiTinh = new JTextField();
+        JButton buttonThemDocGia = new JButton();
+        GraphicFunctions.setLabel(themDocGia, contentPane, "THÊM ĐỘC GIẢ", 90, 20, 120, 20);
+        GraphicFunctions.setLabel(maThe, contentPane, "Mã thẻ", 20, 60, 120, 20);
+        GraphicFunctions.setLabel(ho, contentPane, "Họ", 20, 100, 120, 20);
+        GraphicFunctions.setLabel(ten, contentPane, "Tên", 20, 140, 120, 20);
+        GraphicFunctions.setLabel(gioiTinh, contentPane, "Giới tính", 20, 180, 120, 20);
+        GraphicFunctions.setTextField(nhapMaThe, contentPane, 100, 60, 120, 20);
+        GraphicFunctions.setTextField(nhapHo, contentPane, 100, 100, 120, 20);
+        GraphicFunctions.setTextField(nhapTen, contentPane, 100, 140, 120, 20);
+        GraphicFunctions.setTextField(nhapGioiTinh, contentPane, 100, 180, 120, 20);
+        GraphicFunctions.setButton(buttonThemDocGia, contentPane, "THÊM ĐỘC GIẢ", 55, 220, 150, 30);
+
+        JLabel xoaDocGia = new JLabel();
+        JLabel maTheXoa = new JLabel();
+        JTextField nhapMaTheDeXoa = new JTextField();
+        JButton buttonXoaDocGia = new JButton();
+        GraphicFunctions.setLabel(xoaDocGia, contentPane, "XÓA ĐỘC GIẢ", 90, 280, 120, 20);
+        GraphicFunctions.setLabel(maTheXoa, contentPane, "Mã thẻ", 20, 310, 120, 20);
+        GraphicFunctions.setTextField(nhapMaTheDeXoa, contentPane, 100, 310, 120, 20);
+        GraphicFunctions.setButton(buttonXoaDocGia, contentPane, "XÓA ĐỘC GIẢ", 55, 350, 150, 30);
+
+        JTable table = new JTable();
+        String column[] = { "MÃ THẺ", "HỌ", "TÊN", "GIỚI TÍNH", "TRẠNG THÁI" };
+        String data[][] = {
+                { "1", "Dang", "Bao", "Nam", "Đang hoạt động" },
+                { "2", "Dangg", "Bao0", "Nam", "Đang hoạt động" } };
+
+        GraphicFunctions.setTable(table, contentPane, column, data);
+        JScrollPane scrollPane = new JScrollPane(table);
+        scrollPane.setBounds(320, 20, 600, 460);
+        contentPane.add(scrollPane);
+
         JButton QuayLai = new JButton();
-
-        GraphicFunctions.setButton(ThemDocGia, contentPane, "THÊM ĐỘC GIẢ", 350, 50, 200, 50);
-        ThemDocGia.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("ThemDocGia");
-            }
-        });
-
-        GraphicFunctions.setButton(DanhSachDocGia, contentPane, "DANH SÁCH ĐỘC GIẢ", 350, 120, 200, 50);
-        DanhSachDocGia.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("DanhSachDocGia");
-            }
-        });
-
-        GraphicFunctions.setButton(XoaDocGia, contentPane, "XÓA ĐỘC GIẢ", 350, 190, 200, 50);
-        XoaDocGia.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("XoaDocGia");
-            }
-        });
-
-        GraphicFunctions.setButton(ChinhSuaDocGia, contentPane, "CHỈNH SỬA ĐỘC GIẢ ", 350, 260, 200, 50);
-        ChinhSuaDocGia.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("ChinhSuaDocGia");
-            }
-        });
-
-        GraphicFunctions.setButton(QuayLai, contentPane, "QUAY LẠI", 350, 330, 200, 50);
+        GraphicFunctions.setButton(QuayLai, contentPane, "QUAY LẠI", 20, 430, 200, 50);
         QuayLai.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 mainMenu();
--- src/schema/DocGia.java
+++ src/schema/DocGia.java
@@ -1,13 +1,13 @@
 package schema;
 
 public class DocGia {
-    private int maThe;
+    private String maThe;
     private String ho;
     private String ten;
     private String gioiTinh;
-    private int trangThai;
+    private String trangThai;
 
-    public void themDocGia(int maThe, String ho, String ten, String gioiTinh) {
+    public void themDocGia(String maThe, String ho, String ten, String gioiTinh) {
         this.setMaThe(maThe);
         this.setHo(ho);
         this.setTen(ten);
@@ -18,11 +18,11 @@ public class DocGia {
         docGia = new DocGia();
     }
 
-    public int getMaThe() {
+    public String getMaThe() {
         return maThe;
     }
 
-    public void setMaThe(int maThe) {
+    public void setMaThe(String maThe) {
         this.maThe = maThe;
     }
 
@@ -50,11 +50,11 @@ public class DocGia {
         this.gioiTinh = gioiTinh;
     }
 
-    public int getTrangThai() {
+    public String getTrangThai() {
         return trangThai;
     }
 
-    public void setTrangThai(int trangThai) {
+    public void setTrangThai(String trangThai) {
         this.trangThai = trangThai;
     }
 
